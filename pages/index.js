54b16cdb7f2fa56820dae2aec5b4aff476cb0544
Honey Lee <104662560+honeyleeuwenhoek@users.noreply.github.com>
2022-06-18 08:44:55 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,23 +28,23 @@ export default function Home() {
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2> The development &rarr;</h2>
+            <p> happenings...</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2> Photos &rarr;</h2>
+            <p> virtual meetings documentation </p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2> COMING SOON &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
